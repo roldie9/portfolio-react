afb071032c5c5a7eb9278ea961a9fed6debcf59f
roldie9 <aroldoreyes9@gmail.com>
2020-05-07 03:28:29 -0500

    modified contact page

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,27 +1,51 @@
-import React from "react"
+import React, { Component } from "react";
  
-function Contact(props) {
-  return (
-    <div>
-      <div className="container contact" /> 
-        <div className="row">
-          <h2 className="col-12 border-bottom border-dark">Contact</h2>
-        </div>
-        <form>  
-          <label for="Name">Name</label>
-          <input type="name" className="form-control" id="nameGoesHere" placeholder="John Smith" />
-              
-          <label for="EmailAddress">Email Address</label>
-          <input type="email" className="form-control" id="emailAddressBox" placeholder="name@example.com" />
-              
-          <label for="Message">Message</label>
-          <textarea className="form-control" id="massageBox" rows="3"></textarea>
-            
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    </div>
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={value: ''};
 
-  )
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+    render(){
+      return (
+        <div className= "wrapper">
+          <div className="col-xs-12 col-sm-6 offset-sm-3">
+            <div className="card">
+            <form className="k-form-inline" onSubmit={this.handleSubmit}>  
+              <label className="k-form-field" for="Name">
+                <span>Name:</span>
+                <input type="text" value={this.state.value}  onChange={this.handleChange} className="k-textbox" placeholder="John Smith" />
+              </label>
+              <br />
+              <label className="k-form-field" for="EmailAddress">
+                <span>Email:</span>
+                <input type="email" value={this.state.value}  onChange={this.handleChange}  className="k-textbox" placeholder="name@example.com" />
+              </label>
+              <br />
+              <label className="k-form-field" for="Message">
+              <span>Message:</span>
+                <textarea value={this.state.value}  onChange={this.handleChange} id="messageBox" rows="3" />
+              </label>
+              <br />
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            </div>
+          </div>
+        </div>
+        
+      )
+      }
 }
  
 export default Contact;
