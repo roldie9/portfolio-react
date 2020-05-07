01b1adf38e93a3bb4379fb3fcf9fc4ba777db137
roldie9 <aroldoreyes9@gmail.com>
2020-05-06 23:31:59 -0500

    about, projects and contact js files pre-modified

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,11 +1,11 @@
 import React from "react"
  
-function NewComponent(props) {
+function Contact(props) {
   return (
     <div>
-      <h1>New Component</h1>
+      <h1>Contact</h1>
     </div>
   )
 }
  
-export default NewComponent;
+export default Contact;
