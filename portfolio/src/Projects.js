dbe8e97a116b114cd9485c06eb2636e6f9970b7f
roldie9 <aroldoreyes9@gmail.com>
2020-05-07 04:04:35 -0500

    removed duplicate props in Project js, no warnings

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -10,14 +10,14 @@ class Projects extends Component {
             </div>
             <div className="row">
                 <div className="col-lg-4 col-sm-6" style={{width: '18rem;'}}>
-                  <img className="img-fluid" alt="SmorgasChord" src={require("./img/smorgaschord.png")} className="card-img-top" />
+                  <img className="img-fluid" alt="SmorgasChord" src={require("./img/smorgaschord.png")} />
                   <div className="card-body">
                     <p className="card-text"><a href="https://kkwoka.github.io/Smorgas-Chord/">Group Project:Smorgas-Chord</a></p>
                     <p className="card-text"><a href="https://github.com/verypaleale/Smorgas-Chord">GitHub Link</a></p>
                   </div>
                 </div>
                 <div className="col-lg-4 col-sm-6" style={{width: '18rem;'}}>
-                  <img className="img-fluid" alt="workdaySched" src={require("./img/workdayscheduler.png")} className="card-img-top" />
+                  <img className="img-fluid" alt="workdaySched" src={require("./img/workdayscheduler.png")} />
                   <div className="card-body">
                     <p className="card-text"><a href="https://roldie9.github.io/work-day-scheduler-HW5/">Workday Scheduler</a></p>
                     <p className="card-text"><a href="https://github.com/roldie9/work-day-scheduler-HW5">GitHub Link</a></p>
@@ -26,14 +26,14 @@ class Projects extends Component {
             </div>
             <div className="row">
                 <div className="col-lg-4 col-sm-6" style={{width: '18rem;'}}>
-                  <img className="img-fluid" alt="passwrodGen" src={require("./img/passwordgen.png")} className="card-img-top" />
+                  <img className="img-fluid" alt="passwrodGen" src={require("./img/passwordgen.png")} />
                   <div className="card-body">
                     <p className="card-text"><a href="https://roldie9.github.io/password-generator-hw3/">Password Generator</a></p>
                     <p className="card-text"><a href="https://github.com/roldie9/password-generator-hw3">GitHub Link</a></p>
                   </div>
                 </div>
                 <div className="col-lg-4 col-sm-6" style={{width: '18rem;'}}>
-                  <img className="img-fluid" alt="quizGen" src={require("./img/quiz.png")} className="card-img-top" />
+                  <img className="img-fluid" alt="quizGen" src={require("./img/quiz.png")} />
                   <div className="card-body">
                     <p className="card-text"><a href="https://roldie9.github.io/code-quiz-hw4/">Pop Quiz!</a></p>
                     <p className="card-text"><a href="https://github.com/roldie9/code-quiz-hw4">GitHub Link</a></p>
@@ -42,7 +42,7 @@ class Projects extends Component {
             </div>
             <div className="row">
                 <div className="col-lg-4 col-sm-6" style={{width: '18rem;'}}>
-                  <img className="img-fluid" alt="emptyProject" src={require("./img/4.jpg")} className="card-img-top" />
+                  <img className="img-fluid" alt="emptyProject" src={require("./img/4.jpg")} />
                   <div className="card-body">
                     <p className="card-text">Project no.5</p>
                   </div>
